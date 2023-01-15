4a3fff477e34fcd2d4fca2889da4d71b7ad93c89
Ashish_11 <ashishkumard1011@gmail.com>
2023-01-15 11:36:23 +0530

    Added all Final files and Database

--- ecommerce.sql
+++ ecommerce.sql
@@ -1,190 +1,300 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ecommerce
+-- ------------------------------------------------------
+-- Server version	8.0.31
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-/*DATABASE AUTO SPARE PART MANAGEMENT
-***************************************/
+--
+-- Table structure for table `category`
+--
 
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `CATEGORY_ID` int NOT NULL AUTO_INCREMENT,
+  `CNAME` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`CATEGORY_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=106 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE  type  (
-   TYPE_ID  int(11)  ,
-   TYPE  varchar(50)  
-)  ;
+--
+-- Dumping data for table `category`
+--
 
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (101,'Books'),(102,'Appliances'),(103,'Mobiles'),(104,'Fashion'),(105,'Healthcare');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
 
-INSERT INTO  type  ( TYPE_ID ,  TYPE ) VALUES
-(1, 'Admin'),
-(2, 'User');
+--
+-- Table structure for table `customer`
+--
 
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer` (
+  `CUST_ID` int NOT NULL AUTO_INCREMENT,
+  `FIRST_NAME` varchar(50) DEFAULT NULL,
+  `LAST_NAME` varchar(50) DEFAULT NULL,
+  `PHONE_NUMBER` varchar(11) DEFAULT NULL,
+  PRIMARY KEY (`CUST_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE  users  (
-   ID  int(11)  ,
-   EMPLOYEE_ID  int(11)  ,
-   USERNAME  varchar(50)  ,
-   PASSWORD  varchar(50)  ,
-   TYPE_ID  int(11)  
-)  ;
+--
+-- Dumping data for table `customer`
+--
 
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES (1,'John','F','1111111111');
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE customer (
-  CUST_ID int(11),
-  FIRST_NAME varchar(50),
-  LAST_NAME varchar(50),
-  PHONE_NUMBER varchar(11)
-);
+--
+-- Table structure for table `employee`
+--
 
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee` (
+  `EMPLOYEE_ID` int NOT NULL AUTO_INCREMENT,
+  `FIRST_NAME` varchar(50) DEFAULT NULL,
+  `LAST_NAME` varchar(50) DEFAULT NULL,
+  `GENDER` varchar(50) DEFAULT NULL,
+  `EMAIL` varchar(100) DEFAULT NULL,
+  `PHONE_NUMBER` varchar(11) DEFAULT NULL,
+  PRIMARY KEY (`EMPLOYEE_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE employee (
-  EMPLOYEE_ID int(11),
-  FIRST_NAME varchar(50),
-  LAST_NAME varchar(50),
-  GENDER varchar(50),
-  EMAIL varchar(100),
-  PHONE_NUMBER varchar(11)
-);
+--
+-- Dumping data for table `employee`
+--
 
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (1,'Ashish','Kumar','Male','ashish@gmail.com','1010101010'),(2,'PNV','Shravan','Male','pnvs@gmail.com','1234567890'),(3,'Arcana','Jane','Female','arcana20@gmail.com','9495347812');
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE  manager  (
-   FIRST_NAME  varchar(50),
-   LAST_NAME  varchar(50),
-   EMAIL  varchar(50),
-   PHONE_NUMBER  varchar(11)
-);
+--
+-- Table structure for table `manager`
+--
 
+DROP TABLE IF EXISTS `manager`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `manager` (
+  `FIRST_NAME` varchar(50) DEFAULT NULL,
+  `LAST_NAME` varchar(50) DEFAULT NULL,
+  `EMAIL` varchar(50) DEFAULT NULL,
+  `PHONE_NUMBER` varchar(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE  product  (
-   PRODUCT_ID  int(11),
-   PRODUCT_CODE  varchar(20),
-   NAME  varchar(50),
-   DESCRIPTION  varchar(250)  ,
-   PRICE  int(50)  ,
-   CATEGORY_ID  int(11)  ,
-   SUPPLIER_ID  int(11)  
-)  ;
+--
+-- Dumping data for table `manager`
+--
 
+LOCK TABLES `manager` WRITE;
+/*!40000 ALTER TABLE `manager` DISABLE KEYS */;
+/*!40000 ALTER TABLE `manager` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE category (
-  CATEGORY_ID int(11),
-  CNAME  varchar(50) 
-);
+--
+-- Table structure for table `product`
+--
 
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `PRODUCT_ID` int NOT NULL AUTO_INCREMENT,
+  `PRODUCT_CODE` varchar(20) DEFAULT NULL,
+  `NAME` varchar(50) DEFAULT NULL,
+  `DESCRIPTION` varchar(250) DEFAULT NULL,
+  `PRICE` int DEFAULT NULL,
+  `CATEGORY_ID` int DEFAULT NULL,
+  `SUPPLIER_ID` int DEFAULT NULL,
+  PRIMARY KEY (`PRODUCT_ID`),
+  KEY `product_fk_1` (`CATEGORY_ID`),
+  KEY `product_fk_2` (`SUPPLIER_ID`),
+  CONSTRAINT `product_fk_1` FOREIGN KEY (`CATEGORY_ID`) REFERENCES `category` (`CATEGORY_ID`),
+  CONSTRAINT `product_fk_2` FOREIGN KEY (`SUPPLIER_ID`) REFERENCES `supplier` (`SUPPLIER_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=148 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE  supplier  (
-   SUPPLIER_ID  int(11)  ,
-   COMPANY_NAME  varchar(50)  ,
-   LOCATION_ID  int(11)  ,
-   PHONE_NUMBER  varchar(11)  
-)  ;
+--
+-- Dumping data for table `product`
+--
 
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (121,'BOOK121','The Subtle Art of Not Giving a F**k','',243,101,11),(122,'BOOK122','The Pyschology of Money','Self-help',225,101,11),(124,'APPLIA124','Bajaj ATX 4 750-watt','Toaster',1595,102,12),(125,'SPHONE125','Redmi Note 7 Pro','Smart Phone',13999,103,13),(126,'SPHONE126','Oneplus Nord','Smart Phone',20999,103,13),(127,'CLOTH127','Van Heusen Men\'s Polo Shirt','Polo Shirt',499,104,14),(128,'CLOTH128','U.S POLO ASSN. Men Casual Pants','Casual Shirt',599,104,14),(129,'MEDI129','Volini Gel 75g','Relief Gel',209,105,15),(130,'MEDI130','Dolo 650','Tablets',29,105,15);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `supplier`
+--
 
-/*Table structure for table  transaction */
+DROP TABLE IF EXISTS `supplier`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `supplier` (
+  `SUPPLIER_ID` int NOT NULL AUTO_INCREMENT,
+  `COMPANY_NAME` varchar(50) DEFAULT NULL,
+  `PHONE_NUMBER` varchar(11) DEFAULT NULL,
+  PRIMARY KEY (`SUPPLIER_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `supplier`
+--
 
-CREATE TABLE  transaction  (
-   TRANS_ID  int(50)  ,
-   CUST_ID  int(11)  ,
-   NUMOFITEMS  varchar(250)  ,
-   SUBTOTAL  varchar(50)  ,
-   CASH  varchar(250)  ,
-   DATE  varchar(50)
-)  ;
+LOCK TABLES `supplier` WRITE;
+/*!40000 ALTER TABLE `supplier` DISABLE KEYS */;
+INSERT INTO `supplier` VALUES (11,'Maharaja Enterprises','7123456789'),(12,'Rajesh Electronics','7234567891'),(13,'Bajaj Electronics','7345678910'),(14,'Vamsi Clothing','7456789123'),(15,'Supriya Medicals','7567890123');
+/*!40000 ALTER TABLE `supplier` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `transaction`
+--
 
-CREATE TABLE  transaction_details  (
-   ID  int(11)  ,
-   PRODUCTS  varchar(250)  ,
-   QTY  varchar(250)  ,
-   PRICE  varchar(250)  ,
-   EMPLOYEE  varchar(250)  
-     
-)  ;
+DROP TABLE IF EXISTS `transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transaction` (
+  `TRANS_ID` int NOT NULL AUTO_INCREMENT,
+  `CUST_ID` int DEFAULT NULL,
+  `NUMOFITEMS` varchar(250) DEFAULT NULL,
+  `SUBTOTAL` varchar(50) DEFAULT NULL,
+  `CASH` varchar(250) DEFAULT NULL,
+  `DATE` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`TRANS_ID`),
+  KEY `transaction_fk_3` (`CUST_ID`),
+  CONSTRAINT `transaction_fk_3` FOREIGN KEY (`CUST_ID`) REFERENCES `customer` (`CUST_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `transaction`
+--
 
+LOCK TABLES `transaction` WRITE;
+/*!40000 ALTER TABLE `transaction` DISABLE KEYS */;
+INSERT INTO `transaction` VALUES (2,1,'2','','14242.00','011563911');
+/*!40000 ALTER TABLE `transaction` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*PRIMARY KEY CONSTRAINTS*/
+--
+-- Table structure for table `transaction_details`
+--
 
+DROP TABLE IF EXISTS `transaction_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transaction_details` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `DATE` varchar(250) DEFAULT NULL,
+  `PRODUCTS` varchar(250) DEFAULT NULL,
+  `QTY` varchar(250) DEFAULT NULL,
+  `PRICE` varchar(250) DEFAULT NULL,
+  `EMPLOYEE` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-ALTER TABLE  category 
-  ADD PRIMARY KEY ( CATEGORY_ID );
+--
+-- Dumping data for table `transaction_details`
+--
 
-ALTER TABLE  customer 
-  ADD PRIMARY KEY ( CUST_ID );
+LOCK TABLES `transaction_details` WRITE;
+/*!40000 ALTER TABLE `transaction_details` DISABLE KEYS */;
+INSERT INTO `transaction_details` VALUES (1,'011563911','The Subtle Art of Not Giving a F**k','1','243','Ashish'),(2,'011563911','Redmi Note 7 Pro','1','13999','Ashish');
+/*!40000 ALTER TABLE `transaction_details` ENABLE KEYS */;
+UNLOCK TABLES;
 
-ALTER TABLE  employee 
-  ADD PRIMARY KEY ( EMPLOYEE_ID );
- 
-ALTER TABLE  product 
-  ADD PRIMARY KEY ( PRODUCT_ID );
+--
+-- Table structure for table `type`
+--
 
-ALTER TABLE  supplier 
-  ADD PRIMARY KEY ( SUPPLIER_ID );
+DROP TABLE IF EXISTS `type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `type` (
+  `TYPE_ID` int NOT NULL,
+  `TYPE` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`TYPE_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-ALTER TABLE  transaction 
-  ADD PRIMARY KEY ( TRANS_ID );
+--
+-- Dumping data for table `type`
+--
 
-ALTER TABLE  transaction_details 
-  ADD PRIMARY KEY ( ID );
+LOCK TABLES `type` WRITE;
+/*!40000 ALTER TABLE `type` DISABLE KEYS */;
+INSERT INTO `type` VALUES (1,'Admin'),(2,'User');
+/*!40000 ALTER TABLE `type` ENABLE KEYS */;
+UNLOCK TABLES;
 
-ALTER TABLE  type 
-  ADD PRIMARY KEY ( TYPE_ID );
+--
+-- Table structure for table `users`
+--
 
-ALTER TABLE  users 
-  ADD PRIMARY KEY ( ID );
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `EMPLOYEE_ID` int DEFAULT NULL,
+  `USERNAME` varchar(50) DEFAULT NULL,
+  `PASSWORD` varchar(50) DEFAULT NULL,
+  `TYPE_ID` int DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `users_fk_3` (`TYPE_ID`),
+  KEY `users_fk_4` (`EMPLOYEE_ID`),
+  CONSTRAINT `users_fk_3` FOREIGN KEY (`TYPE_ID`) REFERENCES `type` (`TYPE_ID`),
+  CONSTRAINT `users_fk_4` FOREIGN KEY (`EMPLOYEE_ID`) REFERENCES `employee` (`EMPLOYEE_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `users`
+--
 
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,1,'Ashish','ashish123',1),(2,2,'pnv','123456',1),(3,3,'arcana20','arcana20',2);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-/*AUTO_INCREMENT for table  category */
-
-ALTER TABLE  users 
-  MODIFY  ID  int(11)   AUTO_INCREMENT;
-
-ALTER TABLE  category 
-  MODIFY  CATEGORY_ID  int(11)   AUTO_INCREMENT;
-
-ALTER TABLE  customer 
-  MODIFY  CUST_ID  int(11)   AUTO_INCREMENT;
-
-ALTER TABLE  employee 
-  MODIFY  EMPLOYEE_ID  int(11)   AUTO_INCREMENT;
-
-ALTER TABLE  supplier 
-  MODIFY  SUPPLIER_ID  int(11)   AUTO_INCREMENT;
-
-ALTER TABLE  product 
-  MODIFY  PRODUCT_ID  int(11)   AUTO_INCREMENT;
-
-ALTER TABLE  transaction 
-  MODIFY  TRANS_ID  int(50)   AUTO_INCREMENT;
-
-ALTER TABLE  transaction_details 
-  MODIFY  ID  int(11)   AUTO_INCREMENT;
-
-
-
-/*Constraints*/
-
-ALTER TABLE  product 
-  ADD CONSTRAINT  product_fk_1  FOREIGN KEY ( CATEGORY_ID ) REFERENCES  category  ( CATEGORY_ID ),
-  ADD CONSTRAINT  product_fk_2  FOREIGN KEY ( SUPPLIER_ID ) REFERENCES  supplier  ( SUPPLIER_ID );
-
-
-ALTER TABLE  transaction 
-  ADD CONSTRAINT  transaction_fk_3  FOREIGN KEY ( CUST_ID ) REFERENCES  customer  ( CUST_ID );
-
-ALTER TABLE  users 
-  ADD CONSTRAINT  users_fk_3  FOREIGN KEY ( TYPE_ID ) REFERENCES  type  ( TYPE_ID ),
-  ADD CONSTRAINT  users_fk_4  FOREIGN KEY ( EMPLOYEE_ID ) REFERENCES  employee  ( EMPLOYEE_ID );
-
-
-INSERT INTO `type` (`TYPE_ID`, `TYPE`) VALUES
-(1, 'Admin'),
-(2, 'User');
-
-INSERT INTO `employee` (`EMPLOYEE_ID`, `FIRST_NAME`, `LAST_NAME`, `GENDER`, `EMAIL`, `PHONE_NUMBER`) VALUES
-(1, 'Ashish', 'Kumar', 'Male', 'ashish@gmail.com', '1010101010'),
-(2, 'PNV', 'Shravan', 'Male', 'pnvs@gmail.com', '1234567890');
-
-INSERT INTO `users` (`ID`, `EMPLOYEE_ID`, `USERNAME`, `PASSWORD`, `TYPE_ID`) VALUES
-(1, 1, 'Ashish', 'ashish123', 1),
-(2, 2, 'pnv', '123456', 2);
-
+-- Dump completed on 2023-01-15 11:33:12
